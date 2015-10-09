f4bb6b46433d8827a43d48772e80033e6a58322c
phobos101 <robwilson101@gmail.com>
2015-10-09 13:28:58 +0100

    Created dynamic player HP bar

--- js/main.js
+++ js/main.js
@@ -4,35 +4,36 @@
 $(start)
 
 function start() {
-  var curNum = 0
-  var player_current_hp = 10;
-  var player_max_hp = 10;
-  var player_current_energy = 10;
-  var player_max_energy = 10;
-  var player_xp = 0;
-  var player_level = 1;
-  var difficulty = 1;
-  var player_gold = 0;
+  var curNum              = 0
+  var playerCurrentHp     = 70;
+  var playerMaxHp         = 100;
+  var playerCurrentEnergy = 10;
+  var playerMaxEnergy     = 10;
+  var playerXp            = 0;
+  var playerLevel         = 1;
+  var difficulty          = 1;
+  var playerGold          = 0;
 
-  updateTop();
+  updateStats();
   setInterval(displayNextImage, 1000);
 
   function displayNextImage() {
     if (curNum === 0) {
-      $('#content').css("background-image", "url('assets/background1.png')")
+      $('#content').css('background-image', 'url("assets/background1.png")')
       curNum = 1;
     }
     else {
-      $('#content').css("background-image", "url('assets/background2.png')")
+      $('#content').css('background-image', 'url("assets/background2.png")')
       curNum = 0;
     } 
   }
 
-  function updateTop() {
-    $("#player_hp").html("HP: " + player_current_hp + " / " + player_max_hp)
-    $("#player_energy").html("Energy: " + player_current_energy + " / " + player_max_energy)
-    $("#level").html("Level: " + player_level)
-    $("#xp_bar").html(player_xp + " / " + (1000 * (player_level * 2)))
+  function updateStats() {
+    $('#player-health-bar').css('width', playerCurrentHp + '%')
+    $('#player_hp').html('HP: ' + playerCurrentHp + ' / ' + playerMaxHp)
+    $('#player_energy').html('Energy: ' + playerCurrentEnergy + ' / ' + playerMaxEnergy)
+    $('#level').html('Level: ' + playerLevel)
+    $('#xp_bar').html(playerXp + ' / ' + (1000 * (playerLevel * 2)))
   }
 
 }
