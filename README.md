fe4aa8e9650bbcad957961b5c074a75ddb880e08
phobos101 <robwilson101@gmail.com>
2015-10-09 16:02:20 +0100

    Updated the game to be outside, Minimilized UI, focus on tablet display

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Dwarven Defender!
 
-TODO HERE
+Clicker Game.
+
+Enemy starts on the right and propressivly moves towards the player
+
+Once the enemy gets close, it will damage the player.
+
+When an enemy is killed it is removed from the game and drops some gold. 
+The enemy also grants XP that will improve the players stats.
+A new enemy is then spawned and this repeats in this fasion.
+
+Gold is used to purchase upgrades and abilities.
+
+When the player dies the game is over.
--- js/main.js
+++ js/main.js
@@ -15,20 +15,7 @@ function start() {
   var playerGold          = 0;
 
   updateStats();
-  setInterval(displayNextImage, 1000);
-
-  var $enemy = $('#enemy').on("click", hitEnemy) 
-
-  function displayNextImage() {
-    if (curNum === 0) {
-      $('#content').css('background-image', 'url("assets/background1.png")')
-      curNum = 1;
-    }
-    else {
-      $('#content').css('background-image', 'url("assets/background2.png")')
-      curNum = 0;
-    } 
-  }
+  var newEnemy = new Enemy;
 
   function updateStats() {
     $('#player-health-bar').css('width', playerCurrentHp + '%')
@@ -39,10 +26,32 @@ function start() {
     $('#xp-bar').html(playerXp + ' / ' + (1000 * (playerLevel * 2)))
   }
 
-  function hitEnemy() {
+  function Enemy() {
+    // this.name = Enemy.getName()
+    this.hp   = 100;
+
+    $('#enemy').on("click", Enemy.hitEnemy) 
+  }
+
+  Enemy.getName = function() {
+    var enemyNames = [
+                      "Xarg",
+                      "Jerxag",
+                      "Slath",
+                      "Laoth",
+                      "glatt"
+                     ]
+    return enemyNames[Math.floor(Math.random()*5)]
+  }
+
+  Enemy.hitEnemy = function() {
     console.log("enemy hit!")
-    enemyCurrentHp -= 10
-    updateStats()
+    this.hp -= 10
+
+    // updateStats()
+    // if (enemyCurrentHp == 0) {
+    //   // Kill enemy
+    // }
   }
 
 }
