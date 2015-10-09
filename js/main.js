effe8d77b0309e7f442ffa04683f3cd8407e7333
phobos101 <robwilson101@gmail.com>
2015-10-09 12:47:40 +0100

    Dynamic XP and stats bar

--- js/main.js
+++ js/main.js
@@ -5,6 +5,16 @@ $(start)
 
 function start() {
   var curNum = 0
+  var player_current_hp = 10;
+  var player_max_hp = 10;
+  var player_current_energy = 10;
+  var player_max_energy = 10;
+  var player_xp = 0;
+  var player_level = 1;
+  var difficulty = 1;
+  var player_gold = 0;
+
+  updateTop();
   setInterval(displayNextImage, 1000);
 
   function displayNextImage() {
@@ -18,5 +28,11 @@ function start() {
     } 
   }
 
-  
+  function updateTop() {
+    $("#player_hp").html("HP: " + player_current_hp + " / " + player_max_hp)
+    $("#player_energy").html("Energy: " + player_current_energy + " / " + player_max_energy)
+    $("#level").html("Level: " + player_level)
+    $("#xp_bar").html(player_xp + " / " + (1000 * (player_level * 2)))
+  }
+
 }
