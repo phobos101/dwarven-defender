93a66887dd01cd75c867a66db4d84b9451c484e8
phobos101 <robwilson101@gmail.com>
2015-10-09 19:01:54 +0100

    Move enemy towards player

--- js/main.js
+++ js/main.js
@@ -31,20 +31,41 @@ Game.prototype.updateStats = function () {
 Game.prototype.spawnEnemy = function(level) {
   var newEnemy = new Enemy(level);
   console.log(newEnemy)
+
+  Game.prototype.despawnEnemy = function() {
+    if (document.getElementById('enemy-health-bar').getAttribute('style') == 'width: 0%;') {
+      console.log("Kill it!")
+      newEnemy.cleanUp();
+      newEnemy = undefined;
+      delete newEnemy;
+      console.log(newEnemy)
+      $('#enemy-health-bar').css('width', '100%')
+    }
+  }
 }
+
 
 //-------------------------------------------
 //              ENEMY OBJECT
 //-------------------------------------------
 
 function Enemy(level) {
-  this.name       = this.getName()
-  this.hp         = 100 * level; // 100 * playerLevel
-  this.$avatar    = $('#enemy')
-  this.$healthBar = $('#enemy-health-bar')
+  this.name        = this.getName();
+  this.playerLevel = level;
+  this.hp          = 100; // 100 * playerLevel
+  this.pos         = 50;
+  this.$avatar     = $('#enemy');
+  this.$healthBar  = $('#enemy-health-bar');
 
-  this.$avatar.attr('src', 'assets/enemy_side_transparent.gif')
-  this.$avatar.on("click", this.click.bind(this)) // Must bind to pass enemy and not the picture!
+  this.$avatar.attr('src', 'assets/enemy_side_transparent.gif');
+  this.$avatar.on("click", this.click.bind(this)); // Must bind to pass enemy and not the picture!
+
+  this.moveTimer = setInterval(this.move.bind(this), 100);
+}
+
+Enemy.prototype.move = function() {
+  this.pos += 1;
+  this.$avatar.css("right", this.pos);
 }
 
 Enemy.prototype.getName = function() {
@@ -57,11 +78,28 @@ Enemy.prototype.getName = function() {
                     "Jahtg",
                     "Yuarn"
                    ]
-  return enemyNames[Math.floor(Math.random()*7)]
+  return enemyNames[Math.floor(Math.random()*7)];
 }
 
 Enemy.prototype.click = function() {
-  console.log(this.hp)
-  this.hp -= 10
-  this.$healthBar.css('width', this.hp + '%')
+  this.hp -= 10;
+  this.$healthBar.css('width', this.hp + '%');
+
+  if (this.hp <= 0) {
+    Game.prototype.despawnEnemy()
+  }
+
+  console.log(this.hp);
+}
+
+Enemy.prototype.cleanUp = function() {
+  clearInterval(this.moveTimer);
+  this.name = undefined;
+  this.playerLevel = null;
+  this.hp = null;
+  this.pos = null;
+  this.$avatar.attr('src', '');
+  this.$avatar.off;
+  this.$avatar = undefined;
+  this.$healthBar = undefined;
 }
