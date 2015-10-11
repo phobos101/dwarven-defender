34f56891d43cb8a525e7ade5ebb35070de811169
Rob Wilson <robwilson101@gmail.com>
2015-10-11 18:49:15 +0100

    Added Henchmen

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function Game() {
   this.playerMaxHp     = 100
   this.playerXp        = 0
   this.level           = 1
-  this.playerGold      = 0
+  this.playerGold      = 1000000
   this.upgradePoints   = 0
   this.kills           = 0
 
@@ -30,23 +30,26 @@ function Game() {
 
   this.enemyHp         = 100 * this.level
   this.enemyPos        = 80.0
-  this.$statButton     = $('#stats-btn')
-  this.$invButton      = $('#inv-btn')
 
   $('#level').html('Level: ' + this.level)
+  $('#gold').html('Gold: ' + this.playerGold)
 
   this.updateStats()
   this.spawnEnemy()
 
-  this.$statButton.on("click", this.showStats.bind(this))
-  this.$invButton.on("click", this.showInv.bind(this))
+  $('#stats-btn').on("click", this.showStats.bind(this))
+  $('#henchmen-btn').on("click", this.showHenchmen.bind(this))
   $('#enemy').on("click", this.clickEnemy.bind(this))
+
   $('#atk-btn').on('click', this.upgradeAttack.bind(this))
   $('#dfs-btn').on('click', this.upgradeDefence.bind(this))
   $('#dge-btn').on('click', this.upgradeDodge.bind(this))
   $('#prs-btn').on('click', this.upgradePresence.bind(this))
-  $('#dev-kill-player').on('click', this.killPlayer.bind(this))
 
+  $('#darius').on('click', this.buyDarius.bind(this))
+  $('#alaris').on('click', this.buyAlaris.bind(this))
+  $('#terna').on('click', this.buyTerna.bind(this))
+  
   this.damageTimer = setInterval(this.takeDmg.bind(this), 500)
 }
 
@@ -60,79 +63,10 @@ Game.prototype.updateStats = function () {
   $('#player-health-bar').attr('value', this.playerCurrentHp)
 }
 
-Game.prototype.showStats = function() {
-  if (document.getElementById('stats-window').hasAttribute('show')) {
-    // Hide the window if open
-    console.log("Game resumed")
-    $('#stats-window').css('display', 'none')
-    document.getElementById('stats-window').removeAttribute('show')
-    $('#enemy').css('display', 'block')
-    $('#player').css('display', 'block')
-    document.getElementById('enemy').removeAttribute('paused')
-  }
-  else {
-    // Show the stats window and pause the game
-    console.log("Game paused")
-    $('#stats-window').css('display', 'block')
-    $('#stats-window').attr('show', 'on')
-    $('#enemy').css('display', 'none')
-    $('#player').css('display', 'none')
-    $('#enemy').attr('paused', 'on')
-
-    $('#points').html('Points to spend: ' + this.upgradePoints)
-    $('#attack').html('Attack: ' + this.playerAtk)
-    $('#defence').html('Defence: ' + this.playerDfs)
-    $('#dodge').html('Dodge: ' + this.playerDge)
-    $('#presence').html('Presence: ' + this.playerPrs)
-  }
-}
-
-Game.prototype.upgradeAttack = function() {
-  if (this.upgradePoints > 0) {
-    this.upgradePoints -= 1
-    this.playerAtk += 1
-    $('#points').html('Points to spend: ' + this.upgradePoints)
-    $('#attack').html('Attack: ' + this.playerAtk)
-  }
-}
-
-Game.prototype.upgradeDefence = function() {
-  if (this.upgradePoints > 0) {
-    this.upgradePoints -= 1
-    this.playerDfs += 1
-    $('#points').html('Points to spend: ' + this.upgradePoints)
-    $('#defence').html('Defence: ' + this.playerDfs)
-  }
-}
-
-Game.prototype.upgradeDodge = function() {
-  if (this.upgradePoints > 0) {
-    this.upgradePoints -= 1
-    this.playerDge += 0.1
-    $('#points').html('Points to spend: ' + this.upgradePoints)
-    $('#dodge').html('Dodge: ' + this.playerDge)
-  }
-}
-
-Game.prototype.upgradePresence = function() {
-  if (this.upgradePoints > 0) {
-    this.upgradePoints -= 1
-    this.playerPrs += 0.1
-    $('#points').html('Points to spend: ' + this.upgradePoints)
-    $('#presence').html('Presence: ' + this.playerPrs)
-  }
-}
-
-Game.prototype.showInv = function() { 
-  console.log("Inventory button clicked!")
-}
-
 Game.prototype.takeDmg = function() {
-  var randomNum = (Math.random()*100).toFixed(2)
-  console.log(randomNum)
-
   if (!document.getElementById('enemy').hasAttribute('paused')) {
     if (document.getElementById('enemy').hasAttribute('damage')) {
+      var randomNum = (Math.random()*100).toFixed(2)
       if (randomNum > this.playerDge) {
         console.log('Taking ' + (this.level+5*1.1) + ' damage! Player has ' + this.playerDfs 
                    + ' defence = ' + (this.level - this.playerDfs))
@@ -161,7 +95,140 @@ Game.prototype.gameOver = function() {
 }
 
 //-------------------------------------------
-//              ENEMY THINGS
+//                 BUTTONS 
+//-------------------------------------------
+
+Game.prototype.showStats = function() {
+  if (document.getElementById('stats-window').hasAttribute('show')) {
+    // Hide the window if open
+    console.log("Game resumed")
+    $('#stats-window').css('display', 'none')
+    document.getElementById('stats-window').removeAttribute('show')
+    $('#enemy').css('display', 'block')
+    $('#player').css('display', 'block')
+    $('#henchmen-btn').css('display', 'block')
+    document.getElementById('enemy').removeAttribute('paused')
+  }
+  else {
+    // Show the stats window and pause the game
+    console.log("Game paused")
+    $('#stats-window').css('display', 'block')
+    $('#stats-window').attr('show', 'on')
+    $('#enemy').css('display', 'none')
+    $('#player').css('display', 'none')
+    $('#henchmen-btn').css('display', 'none')
+    $('#enemy').attr('paused', 'on')
+
+    $('#points').html('Points to spend: ' + this.upgradePoints)
+    $('#attack').html('Attack: ' + this.playerAtk)
+    $('#defence').html('Defence: ' + this.playerDfs)
+    $('#dodge').html('Dodge: ' + this.playerDge)
+    $('#presence').html('Presence: ' + this.playerPrs)
+  }
+}
+
+Game.prototype.showHenchmen = function() {
+  if (document.getElementById('henchmen-window').hasAttribute('show')) {
+    // Hide the window if open
+    console.log("Game resumed")
+    $('#henchmen-window').css('display', 'none')
+    document.getElementById('henchmen-window').removeAttribute('show')
+    $('#enemy').css('display', 'block')
+    $('#player').css('display', 'block')
+    $('#stats-btn').css('display', 'block')
+    document.getElementById('enemy').removeAttribute('paused')
+  }
+  else {
+    // Show the stats window and pause the game
+    console.log("Game paused")
+    $('#henchmen-window').css('display', 'block')
+    $('#henchmen-window').attr('show', 'on')
+    $('#enemy').css('display', 'none')
+    $('#player').css('display', 'none')
+    $('#stats-btn').css('display', 'none')
+    $('#enemy').attr('paused', 'on')
+  }
+}
+
+//-------------------------------------------
+//                 UPGRADES 
+//-------------------------------------------
+
+Game.prototype.upgradeAttack = function() {
+  if (this.upgradePoints > 0) {
+    this.upgradePoints -= 1
+    this.playerAtk += 1
+    $('#points').html('Points to spend: ' + this.upgradePoints)
+    $('#attack').html('Attack: ' + this.playerAtk)
+  }
+}
+
+Game.prototype.upgradeDefence = function() {
+  if (this.upgradePoints > 0) {
+    this.upgradePoints -= 1
+    this.playerDfs += 1
+    $('#points').html('Points to spend: ' + this.upgradePoints)
+    $('#defence').html('Defence: ' + this.playerDfs)
+  }
+}
+
+Game.prototype.upgradeDodge = function() {
+  if (this.upgradePoints > 0) {
+    this.upgradePoints -= 1
+    this.playerDge += 0.1
+    $('#points').html('Points to spend: ' + this.upgradePoints)
+    $('#dodge').html('Dodge: ' + this.playerDge.toFixed(2))
+  }
+}
+
+Game.prototype.upgradePresence = function() {
+  if (this.upgradePoints > 0) {
+    this.upgradePoints -= 1
+    this.playerPrs += 0.1
+    $('#points').html('Points to spend: ' + this.upgradePoints)
+    $('#presence').html('Presence: ' + this.playerPrs.toFixed(2))
+  }
+}
+
+//-------------------------------------------
+//                 HENCHMEN 
+//-------------------------------------------
+
+Game.prototype.buyDarius = function() {
+  if (this.playerGold >= 100) {
+    console.log('Darius Hired!')
+    this.dariusTimer = setInterval(this.clickEnemy.bind(this), 1000)
+    this.playerGold -= 100
+    $('#gold').html('Gold: ' + this.playerGold)
+    $('#darius').html('Darius Hired!')
+    $('#darius').off()
+  }
+}
+
+Game.prototype.buyAlaris = function() {
+  if (this.playerGold >= 10000) {
+    console.log('Alaris Hired!')
+    this.alarisTimer = setInterval(this.clickEnemy.bind(this), 200)
+    this.playerGold -= 10000
+    $('#gold').html('Gold: ' + this.playerGold)
+    $('#alaris').html('Alaris Hired!')
+    $('#alaris').off()
+  }
+}
+
+Game.prototype.buyTerna = function() {
+  if (this.playerGold >= 100000) {
+    console.log('Terna Hired!')
+    this.ternaTimer = setInterval(this.clickEnemy.bind(this), 100)
+    this.playerGold -= 100000
+    $('#gold').html('Gold: ' + this.playerGold)
+    $('#terna').html('Terna Hired!')
+    $('#terna').off()
+  }
+}
+
+//-------------------------------------------
+//                  ENEMY 
 //-------------------------------------------
 
 Game.prototype.spawnEnemy = function() {
@@ -209,24 +276,29 @@ Game.prototype.moveEnemy = function() {
 }
 
 Game.prototype.clickEnemy = function() {
-  if (this.enemyHp > 0) {
-    this.enemyHp -= this.playerAtk;
+  if (!document.getElementById('enemy').hasAttribute('paused')) {
     if (this.enemyHp > 0) {
-      $('#enemy-health-bar').attr('value', this.enemyHp)
+      this.enemyHp -= this.playerAtk;
+      if (this.enemyHp > 0) {
+        $('#enemy-health-bar').attr('value', this.enemyHp)
+      }
+      else {
+        var randomNum = Math.ceil(Math.random()*20)
+        $('#enemy-health-bar').attr('value', 0)
+        console.log("Death!")
+        document.getElementById('enemy').removeAttribute('damage')
+        this.kills++
+        this.playerGold += this.level * randomNum
+        $('#gold').html('Gold: ' + this.playerGold)
+        $('#enemy').fadeOut(1000)
+        setTimeout(this.spawnEnemy.bind(this), 1100)
+        clearInterval(this.moveTimer)
+        this.enemyHp = -1
+      }
+      console.log(this.enemyHp)
     }
     else {
-      $('#enemy-health-bar').attr('value', 0)
-      console.log("Death!")
-      document.getElementById('enemy').removeAttribute('damage')
-      this.kills++
-      $('#enemy').fadeOut(1000)
-      setTimeout(this.spawnEnemy.bind(this), 1100)
-      clearInterval(this.moveTimer)
-      this.enemyHp = -1
+      console.log('Enemy already fading')
     }
-    console.log(this.enemyHp)
   }
-  else {
-    console.log('Enemy already fading')
-  }
-} // End of clickEnemy function
+} 
